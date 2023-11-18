b06069b77e4302ddb07c635c657ca37e4968c16b
PranabKumarSahoo <kumarspranab19@gmail.com>
2023-11-18 23:49:44 +0530

    initial commit

--- src/sections/index.js
+++ src/sections/index.js
@@ -5,7 +5,7 @@ import Services from "./Services";
 import SpecialOffer from "./SpecialOffer";
 import CustomerReviews from "./CustomerReviews";
 import Subscribe from "./Subscribe";
-import Footer from "./footer";
+import Footer from "./Footer";
 
 export {
     Hero,
